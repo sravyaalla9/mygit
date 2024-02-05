aa55dc92a32f02e81d6f799c0caea77e4e9e2fc7
sravya <sravyaalla9@gmail.com>
2024-02-05 12:26:13 +0530

    second version

---
+++ BI FOLDER/DATAS/emp details.sql
@@ -0,0 +1,4 @@
+create database emp_details;
+create table emp(emp_id int,name varchar(60),salery int);
+insert into emp(emp_id,name,salery) values(1,'sravya',1000),(2,'divya',2000);
+select * from emp
